2222c64e342d1ad1a195aba0fcb45fa569c58f9b
Ptar* <peterolwande@gmail.com>
2024-03-18 16:05:54 +0300

    Remove blog slug

--- app/page.js
+++ app/page.js
@@ -10,8 +10,28 @@ export const metadata = {
 };
 
 export default async function Home() {
-  const res = await fetch(baseURL + "/api/blogs");
-  const blogs = res.json();
+  // const res = await fetch(baseURL + "/api/blogs");
+  // const blogs = res.json();
+  const blogs = [
+    {
+      id: 1,
+      title: "Title for blog 1 ",
+      slug: "slug_1",
+      body: "The body for 2nd blog post",
+    },
+    {
+      id: 2,
+      title: "Title for blog 2 ",
+      slug: "slug_2",
+      body: "The body for first blog post",
+    },
+    {
+      id: 3,
+      title: "Title for blog 3 ",
+      slug: "slug_3",
+      body: "The body for 3rd blog post",
+    },
+  ];
   return (
     <main>
       <div>
